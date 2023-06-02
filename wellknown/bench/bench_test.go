8548e1e014fc1f37358b6ad14dc23d4bad1ad13b
John Howard <howardjohn@google.com>
2023-06-02 08:40:21 -0700

    benchmark handwritten

---
+++ wellknown/bench/bench_test.go
@@ -0,0 +1,42 @@
+package wrapperspb
+
+import (
+	"bytes"
+	"testing"
+
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/runtime/protoiface"
+)
+
+func Benchmark(b *testing.B) {
+	input := Double(12345)
+	want, _ := proto.Marshal(input)
+	var got []byte
+	b.Run("std", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			got, _ = proto.Marshal(input)
+		}
+		if !bytes.Equal(want, got) {
+			b.Fatalf("bad value: %v vs %v", want, got)
+		}
+	})
+	b.Run("vt", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			got, _ = input.MarshalVT()
+		}
+		if !bytes.Equal(want, got) {
+			b.Fatalf("bad value: %v vs %v", want, got)
+		}
+	})
+	b.Run("hand", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			gotv, _ := input.ProtoMethods().Marshal(protoiface.MarshalInput{
+				Message: input.ProtoReflect(),
+			})
+			got = gotv.Buf
+		}
+		if !bytes.Equal(want, got) {
+			b.Fatalf("bad value: %v vs %v", want, got)
+		}
+	})
+}
---
+++ wellknown/bench/wrapper_protomethods.go
@@ -0,0 +1,40 @@
+package wrapperspb
+
+import (
+	"encoding/binary"
+	"math"
+
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/runtime/protoiface"
+)
+
+var methods = Marshaller(func(ms protoreflect.ProtoMessage) ([]byte, error) {
+	m := ms.(*DoubleValue)
+	if m == nil {
+		return nil, nil
+	}
+	size := 9
+	d := make([]byte, size)
+	d[0] = 0x9
+	binary.LittleEndian.PutUint64(d[1:], math.Float64bits(m.Value))
+	return d, nil
+})
+
+func (_ *DoubleValue) ProtoMethods() *protoiface.Methods {
+	return methods
+}
+
+func Marshaller(marshal func(message protoreflect.ProtoMessage) ([]byte, error)) *protoiface.Methods {
+	return &protoiface.Methods{
+		Size: nil,
+		Marshal: func(in protoiface.MarshalInput) (protoiface.MarshalOutput, error) {
+			out, err := marshal(in.Message.Interface())
+			if in.Buf != nil {
+				out = append(in.Buf, out...)
+			}
+			return protoiface.MarshalOutput{
+				Buf: out,
+			}, err
+		},
+	}
+}
